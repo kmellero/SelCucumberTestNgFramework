8428d5ee66b2c0db66db94b994a258056bcfbde4
kmellero <ktm000@yahoo.com>
2019-01-10 14:42:15 +0100

    first code

---
+++ src/main/java/com/testautomation/features/YouTubeSearch.feature
@@ -0,0 +1,7 @@
+@UATTesting
+Feature: Youtube Search Validations
+
+  Scenario: Youtube Search positive scenarios
+    Given Open chrome browser and enter url
+    When Enter search criteria
+    Then Click on search button
---
+++ target/site/cucumber-pretty/report.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("YouTubeSearch.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Youtube Search Validations",
+  "description": "",
+  "id": "youtube-search-validations",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@UATTesting"
+    }
+  ]
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Youtube Search positive scenarios",
+  "description": "",
+  "id": "youtube-search-validations;youtube-search-positive-scenarios",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "Open chrome browser and enter url",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "Enter search criteria",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "Click on search button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "YouTubeSearchStepDef.open_chrome_browser_and_enter_url()"
+});
+formatter.result({
+  "duration": 13012065391,
+  "status": "passed"
+});
+formatter.match({
+  "location": "YouTubeSearchStepDef.enter_search_criteria()"
+});
+formatter.result({
+  "duration": 1253688098,
+  "status": "passed"
+});
+formatter.match({
+  "location": "YouTubeSearchStepDef.click_on_search_button()"
+});
+formatter.result({
+  "duration": 4039496149,
+  "status": "passed"
+});
+});
